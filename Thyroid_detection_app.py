45c24fcd5c2ccbe9fb851856a38bb5fff28a6e44
AsishStains <126491249+AsishStains@users.noreply.github.com>
2024-04-07 03:09:03 +0530

    Update Thyroid_detection_app.py

--- Thyroid_detection_app.py
+++ Thyroid_detection_app.py
@@ -4,10 +4,10 @@ import pickle
 import asyncio
 
 class_mapping = {
-    0: 'compensated_thyroid',
+    0: 'compensated_hypothyroid',
     1: 'hyperthyroid',
     2: 'negative',
-    3: 'primary_thyroid'
+    3: 'primary_hypothyroid'
 }
 
 def load_model_and_encoder(model_path, encoder_path):
@@ -23,18 +23,18 @@ def predict_thyroid_class(model, encoder, input_data):
 
 async def main():
     st.title("Thyroid Disease Detection")
-    st.write("Enter the required information below:")
+    st.write("Enter the required information below from the Blood report:")
 
     age = st.number_input("Enter age:", min_value=0, step=1)
     sex = st.radio("Select sex:", ('Female', 'Male'))
-    tsh = st.number_input("Enter TSH value:")
-    t3 = st.number_input("Enter T3 value:")
-    t4 = st.number_input("Enter T4 value:")
+    tsh = st.number_input("Enter TSH value(μIU/ml):")
+    t3 = st.number_input("Enter T3 value(ng/dL):")
+    t4 = st.number_input("Enter T4 value(μg/dL):")
 
     sex_numeric = 0 if sex == 'Female' else 1
     input_data = np.array([[age, sex_numeric, tsh, t3, t4]])
 
-    model_path = "tddmodel.pkl"
+    model_path = "tddmodelml.pkl"
     encoder_path = "encoder.pickle"
     model, encoder = load_model_and_encoder(model_path, encoder_path)
 
